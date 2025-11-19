b36a507ff3f25b1293a048b09df64fc1efb76012
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-11-18 19:03:16 -0800

    Update skin.js

--- resources/skin.js
+++ resources/skin.js
@@ -1,22 +1,19 @@
 /* JavaScript for the Ridvan skin */
 $( function () {
-    
     // 1. Handle "Click Outside" to close menus
     $( document ).on( 'click', function ( e ) {
-        // Check if the click happened INSIDE a portlet/dropdown
+        // Check if the click happened INSIDE a portlet (menu container)
         if ( !$( e.target ).closest( '.mw-portlet' ).length ) {
-            // If not, uncheck ALL dropdown checkboxes
-            $( '.mw-checkbox-hack-checkbox' ).prop( 'checked', false );
+            // If not, find ALL checkboxes inside portlets and uncheck them
+            $( '.mw-portlet input[type="checkbox"]' ).prop( 'checked', false );
         }
     } );
-
     // 2. Handle "Exclusive" behavior (Close others when one opens)
-    $( '.mw-checkbox-hack-checkbox' ).on( 'change', function () {
-        // If this specific checkbox was just checked (menu opened)...
+    $( '.mw-portlet input[type="checkbox"]' ).on( 'change', function () {
+        // If this specific menu was just opened...
         if ( $( this ).is( ':checked' ) ) {
-            // ...find all OTHER checkboxes and uncheck them.
-            $( '.mw-checkbox-hack-checkbox' ).not( this ).prop( 'checked', false );
+            // ...find all OTHER checkboxes and close them.
+            $( '.mw-portlet input[type="checkbox"]' ).not( this ).prop( 'checked', false );
         }
     } );
-
 } );
