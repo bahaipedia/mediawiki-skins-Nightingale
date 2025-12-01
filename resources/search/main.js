c03a37f68347b4c86adfc4c0c09e7b892eb546e5
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-11-30 17:09:43 -0800

    Update main.js

--- resources/search/main.js
+++ resources/search/main.js
@@ -1,12 +1,18 @@
-console.log( 'Ridvan: search2 js loaded successfully.' );
-/**
- * @return {void}
- */
+console.log( 'Ridvan: search js v3 loaded successfully.' );
+
 function initSearchLoader() {
 	const searchForm = document.getElementById( 'searchform' );
 	const searchInput = document.getElementById( 'searchInput' );
 
 	if ( searchForm && searchInput ) {
+		// 1. Kill default MediaWiki search suggestions
+		if ( window.jQuery ) {
+			$( searchInput ).off( 'keydown.suggestions keypress.suggestions keyup.suggestions' );
+			// Depending on MW version, this might also be needed:
+			$( searchInput ).searchSuggest( 'disable' );
+		}
+
+		// 2. Initialize our custom search
 		const typeahead = require( './typeahead.js' );
 		typeahead.init( searchForm, searchInput );
 	}
